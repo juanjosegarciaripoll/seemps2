e09e7d4fde11cda3506a06f7b4d3ee9c651af011
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2023-09-06 10:41:16 +0200

    Create a new registers module, grouping qubit operations

---
+++ seemps/register/__init__.py
@@ -0,0 +1,2 @@
+from .qubo import qubo_exponential_mpo, qubo_mpo
+from .transforms import wavefunction_product, twoscomplement
--- seemps/register/qubo.py
+++ seemps/register/qubo.py
@@ -1,8 +1,7 @@
 import numpy as np
-from .typing import *
-from .state import MPS
-from .mpo import MPOList, MPO
-from . import truncate
+from ..typing import *
+from ..state import MPS
+from ..mpo import MPOList, MPO
 
 
 def qubo_mpo(
@@ -19,7 +18,7 @@ def qubo_mpo(
     h : Optional[Vector] :
         (Default value = None)
     **kwdargs :
-        
+
     J: Optional[Operator] :
          (Default value = None)
     h: Optional[Vector] :
@@ -28,7 +27,7 @@ def qubo_mpo(
     Returns
     -------
 
-    
+
     """
     if J is None:
         #
@@ -88,7 +87,7 @@ def qubo_exponential_mpo(
     beta : float :
         (Default value = -1.0)
     **kwdargs :
-        
+
     J: Optional[Operator] :
          (Default value = None)
     h: Optional[Vector] :
@@ -99,7 +98,7 @@ def qubo_exponential_mpo(
     Returns
     -------
 
-    
+
     """
     if J is None:
         #
@@ -136,144 +135,3 @@ def qubo_exponential_mpo(
             data[-1] = A[:, :, :, [0]] + A[:, :, :, [1]]
             out.append(MPO(data, **kwdargs))
         return MPOList(out)
-
-
-def wavefunction_product(
-    ψ: MPS, ξ: MPS, conjugate: bool = False, simplify: bool = True, **kwdargs
-) -> MPS:
-    """Implement a nonlinear transformation that multiplies two MPS, to
-    create a new MPS with combined bond dimensions. In other words, act
-    with the nonlinear transformation <s|ψξ> = ψ(s)ξ(s)|s> or
-    <s|ψ*ξ> = ψ*(s)ξ(s)|s>
-
-    Parameters
-    ----------
-    ψ :
-        ξ
-    conjugate :
-        Conjugate ψ or not
-    simplify :
-        Simplify the state afterwards or not
-    kwdargs :
-        Arguments to simplify
-    Output :
-        
-    mps :
-        The MPS product ψξ or ψ
-    ψ : MPS :
-        
-    ξ : MPS :
-        
-    conjugate : bool :
-        (Default value = False)
-    simplify : bool :
-        (Default value = True)
-    **kwdargs :
-        
-    ψ: MPS :
-        
-    ξ: MPS :
-        
-    conjugate: bool :
-         (Default value = False)
-    simplify: bool :
-         (Default value = True)
-
-    Returns
-    -------
-
-    
-    """
-
-    def combine(A, B):
-        """
-
-        Parameters
-        ----------
-        A :
-            
-        B :
-            
-
-        Returns
-        -------
-
-        
-        """
-        # Combine both tensors
-        a, d, b = A.shape
-        c, d, e = B.shape
-        if conjugate:
-            A = A.conj()
-        D = np.array(
-            [np.outer(A[:, i, :].reshape(-1), B[:, i, :].reshape(-1)) for i in range(d)]
-        )
-        D = np.einsum("iabce->acibe", np.array(D).reshape(d, a, b, c, e)).reshape(
-            a * c, d, b * e
-        )
-        return D
-
-    out = MPS([combine(A, B) for A, B in zip(ψ, ξ)])
-    if simplify:
-        out, _, _ = truncate.simplify(out, **kwdargs)
-    return out
-
-
-def twoscomplement(
-    L: int, control: int = 0, sites: Optional[Iterable[int]] = None, **kwdargs
-) -> MPO:
-    """Return an MPO that performs a two's complement of the selected qubits
-    depending on a 'control' qubit in a register with L qubits.
-    
-    Arguments
-    ---------
-    L       -- Real size of register
-    control -- Which qubit (relative to sites) controls the sign.
-               Defaults to the first qubit in 'sites'.
-    sites   -- The qubits involved in the MPO. Defaults to range(L).
-    kwdargs -- Arguments for MPO.
-
-    Parameters
-    ----------
-    L : int :
-        
-    control : int :
-        (Default value = 0)
-    sites : Optional[Iterable[int]] :
-        (Default value = None)
-    **kwdargs :
-        
-    L: int :
-        
-    control: int :
-         (Default value = 0)
-    sites: Optional[Iterable[int]] :
-         (Default value = None)
-
-    Returns
-    -------
-
-    
-    """
-
-    if sites is not None:
-        sites = sorted(sites)
-        out = twoscomplement(
-            len(sites), control=sites.index(control), sites=None, **kwdargs
-        )
-        return out.extend(L, sites=sites)
-    else:
-        A0 = np.zeros((2, 2, 2, 2))
-        A0[0, 0, 0, 0] = 1.0
-        A0[1, 1, 1, 1] = 1.0
-        A = np.zeros((2, 2, 2, 2))
-        A[0, 0, 0, 0] = 1.0
-        A[0, 1, 1, 0] = 1.0
-        A[1, 1, 0, 1] = 1.0
-        A[1, 0, 1, 1] = 1.0
-        data = [A0 if i == control else A for i in range(L)]
-        A = data[0]
-        data[0] = A[[0], :, :, :] + A[[1], :, :, :]
-        A = data[-1]
-        data[-1] = A[:, :, :, [0]] + A[:, :, :, [1]]
-        return MPO(data, **kwdargs)
