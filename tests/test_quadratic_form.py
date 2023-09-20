9bc070c5d9d0d2867418c035849f81e7e3ed49fa
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2023-09-20 08:49:56 +0200

    Increase test coverage dmrg.py

--- tests/test_quadratic_form.py
+++ tests/test_quadratic_form.py
@@ -2,7 +2,9 @@ import numpy as np
 from seemps.optimization.dmrg import QuadraticForm, dmrg
 from seemps.hamiltonians import ConstantTIHamiltonian, HeisenbergHamiltonian
 from seemps.state._contractions import _contract_last_and_first
-from seemps.state import random_uniform_mps
+from seemps.state import random_uniform_mps, product_state
+from seemps.mpo import MPO
+from seemps.tools import σx
 from .tools import *
 from seemps.tools import DEBUG
 import scipy.sparse.linalg
@@ -14,6 +16,18 @@ class TestQuadraticForm(TestCase):
 
     def setUp(self) -> None:
         return super().setUp()
+
+    def test_quadratic_form_checks_mpo_size(self):
+        mpo = MPO([np.ones((1, 2, 2, 1))] * 3)
+        mps = random_uniform_mps(2, 4, rng=self.rng)
+        with self.assertRaises(Exception):
+            QuadraticForm(mpo, mps)
+
+    def test_quadratic_form_checks_mpo_dimensions(self):
+        mpo = MPO([np.ones((1, 2, 2, 1))] * 3)
+        mps = random_uniform_mps(3, 3, rng=self.rng)
+        with self.assertRaises(Exception):
+            QuadraticForm(mpo, mps)
 
     def test_quadratic_form_two_sites(self):
         H = ConstantTIHamiltonian(size=2, interaction=np.kron(self.Sz, self.Sx))
@@ -79,7 +93,7 @@ class TestDMRG(TestCase):
         self.assertAlmostEqual(result.energy, -1)
         v = result.state.to_vector()
         self.assertAlmostEqual(v[0] ** 2 + v[3] ** 2, 1.0)
-        self.assertAlmostEqual(v[1] ** 2 + v[2] ** 2, 1.0)
+        self.assertAlmostEqual(v[1] ** 2 + v[2] ** 2, 0.0)
 
     def test_dmrg_on_Ising_two_sites(self):
         """Check we can compute ground state of Sz * Sz on two sites"""
@@ -97,5 +111,23 @@ class TestDMRG(TestCase):
         E, exact_v = scipy.sparse.linalg.eigsh(H.tomatrix(), k=1, which="SA")
         self.assertAlmostEqual(result.energy, E[0])
         v = result.state.to_vector()
-        print(np.linalg.norm(v), np.linalg.norm(exact_v))
         self.assertAlmostEqual(abs(np.vdot(v, exact_v)), 1.0)
+
+    def test_dmrg_works_with_hamiltonian(self):
+        """Check we can compute ground state of Sz * Sz on two sites"""
+        H = ConstantTIHamiltonian(size=3, interaction=-np.kron(self.Sz, self.Sz))
+        Hmpo = H.to_mpo()
+        result = dmrg(H)
+        self.assertAlmostEqual(result.energy, -2)
+        self.assertAlmostEqual(Hmpo.expectation(result.state), -2)
+
+    def test_dmrg_uses_guess_with_canonical_centers_other_than_zero(self):
+        """Check we can compute ground state of Sz * Sz on two sites"""
+        H = ConstantTIHamiltonian(size=2, interaction=-np.kron(self.Sz, self.Sz))
+        mps = CanonicalMPS(product_state([1.0, 1.0], 2), center=-1, normalize=True)
+        Hmpo = H.to_mpo()
+        result = dmrg(Hmpo, guess=mps, maxiter=100)
+        self.assertAlmostEqual(result.energy, -1)
+        v = result.state.to_vector()
+        self.assertAlmostEqual(v[0] ** 2 + v[3] ** 2, 1.0)
+        self.assertAlmostEqual(v[1] ** 2 + v[2] ** 2, 0.0)
