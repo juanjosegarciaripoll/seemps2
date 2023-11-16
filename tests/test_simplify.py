3059ac4f5d648e2f90b31e02fd15d3c8d2de40d3
PaulaGarciaMolina <paulagm12@gmail.com>
2023-11-16 12:32:48 +0100

    Reorder simplify operations.

--- tests/test_simplify.py
+++ tests/test_simplify.py
@@ -14,7 +14,7 @@ class TestSimplify(TestCase):
         for n in range(3,9):
             ψ = random_uniform_mps(d, n, D=int(2**(n/2)))
             ψ = ψ * (1/ψ.norm())
-            φ = simplify(ψ, truncation=NO_TRUNCATION)
+            φ = simplify(ψ, strategy=NO_TRUNCATION)
             self.assertSimilar(ψ.to_vector(), φ.to_vector())
 
     def test_tolerance(self):
@@ -24,7 +24,7 @@ class TestSimplify(TestCase):
         for n in range(3,15):
             ψ = random_uniform_mps(d, n, D=int(2**(n/2)))
             ψ = ψ * (1/ψ.norm())
-            φ = simplify(ψ, truncation=strategy)
+            φ = simplify(ψ, strategy=strategy)
             err = 2 * abs(
             1.0 - scprod(ψ, φ).real / (ψ.norm() * φ.norm()))
             self.assertTrue(err < tolerance)
@@ -36,7 +36,7 @@ class TestSimplify(TestCase):
             strategy = DEFAULT_STRATEGY.replace(max_bond_dimension=D)
             ψ = random_uniform_mps(d, n, D=int(2**(n/2)))
             ψ = ψ * (1/ψ.norm())
-            φ = simplify(ψ, truncation=strategy)
+            φ = simplify(ψ, strategy=strategy)
             max_D_φ = max([max(t.shape) for t in φ])
             self.assertTrue(max_D_φ <= D)
     
@@ -47,8 +47,8 @@ class TestSimplify(TestCase):
         for n in range(3,9):
             ψ = random_uniform_mps(d, n, D=int(2**(n/2)))
             ψ = ψ * (1/ψ.norm())
-            φ0 = simplify(ψ, truncation=strategy_0)
-            φ1 = simplify(ψ, truncation=strategy_1)
-            self.assertSimilar(ψ.to_vector(), φ0.to_vector())
-            self.assertSimilar(ψ.to_vector(), φ1.to_vector())
-            self.assertSimilar(φ0.to_vector(), φ1.to_vector())
+            φ0 = simplify(ψ, strategy=strategy_0)
+            φ1 = simplify(ψ, strategy=strategy_1)
+            self.assertSimilar(ψ, φ0)
+            self.assertSimilar(ψ, φ1)
+            self.assertSimilar(φ0, φ1)
