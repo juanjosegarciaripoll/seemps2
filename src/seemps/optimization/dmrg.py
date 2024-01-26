5ff642d5b166be8ea65f09be432e41c62073d100
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2024-01-26 22:52:56 +0100

    DMRG computes variance and uses new callback

--- src/seemps/optimization/dmrg.py
+++ src/seemps/optimization/dmrg.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Callable
 
 import numpy as np
@@ -100,9 +101,10 @@ class QuadraticForm:
 def dmrg(
     H: Union[MPO, NNHamiltonian],
     guess: Optional[MPS] = None,
-    strategy: Strategy = DEFAULT_STRATEGY,
-    tol: float = 1e-10,
     maxiter: int = 20,
+    tol: float = 1e-10,
+    tol_up: Optional[float] = None,
+    strategy: Strategy = DEFAULT_STRATEGY,
     callback: Optional[Callable] = None,
 ) -> OptimizeResults:
     """Compute the ground state of a Hamiltonian represented as MPO using the
@@ -115,15 +117,17 @@ def dmrg(
         nearest-neighbor Hamiltonian that is implicitly converted to MPO.
     guess : Optional[MPS]
         An initial guess for the ground state.
-    strategy : Strategy
-        Truncation strategy to keep bond dimensions in check. Defaults to
-        `DEFAULT_STRATEGY`, which is very strict.
-    tol : float
-        Tolerance in the energy to detect convergence of the algorithm.
     maxiter : int
         Maximum number of steps of the DMRG. Each step is a sweep that runs
         over every pair of neighborin sites. Defaults to 20.
-    callback : Optional[callable]
+    tol : float
+        Tolerance in the energy to detect convergence of the algorithm.
+    tol_up : float, default = `tol`
+        If energy fluctuates up below this tolerance, continue the optimization.
+    strategy : Strategy
+        Truncation strategy to keep bond dimensions in check. Defaults to
+        `DEFAULT_STRATEGY`, which is very strict.
+    callback : Optional[Callable[[MPS, OptimizeResults], Any]]
         A callable called after each iteration (defaults to None).
 
     Returns
@@ -143,6 +147,8 @@ def dmrg(
         H = H.to_mpo()
     if guess is None:
         guess = random_mps(H.dimensions(), D=2)
+    if tol_up is None:
+        tol_up = abs(tol)
 
     if not isinstance(guess, CanonicalMPS):
         guess = CanonicalMPS(guess, center=0)
@@ -152,55 +158,57 @@ def dmrg(
     else:
         direction = -1
         QF = QuadraticForm(H, guess, start=H.size - 2)
-    best_energy = np.Inf
-    best_vector = guess
-    oldE = np.inf
-    energies = []
-    converged = False
-    msg = "DMRG did not converge"
+    energy = H.expectation(QF.state)
+    variance = abs(H.apply(QF.state).norm_squared() - energy * energy)
+    results = OptimizeResults(
+        state=guess,
+        energy=np.Inf,
+        converged=False,
+        message=f"Exceeded maximum number of steps {maxiter}",
+        trajectory=[energy],
+        variances=[variance],
+    )
+    if callback is not None:
+        callback(QF.state, results)
+    E: float = np.Inf
+    last_E: float = E
     strategy = strategy.replace(normalize=True)
     for step in range(maxiter):
         if direction > 0:
             for i in range(0, H.size - 1):
-                newE, AB = QF.diagonalize(i)
+                E, AB = QF.diagonalize(i)
                 QF.update_2site_right(AB, i, strategy)
-                log(f"-> site={i}, energy={newE}, {H.expectation(QF.state)}")
+                log(f"-> site={i}, eigenvalue={E}")
         else:
             for i in range(H.size - 2, -1, -1):
-                newE, AB = QF.diagonalize(i)
+                E, AB = QF.diagonalize(i)
                 QF.update_2site_left(AB, i, strategy)
-                log(f"<- site={i}, energy={newE}, {H.expectation(QF.state)}")
+                log(f"<- site={i}, eigenvalue={E}")
 
+        # In principle, E is the exact eigenvalue. However, we have
+        # truncated the eigenvector, which means that the computation of
+        # the residual cannot use that value
+        energy = H.expectation(QF.state)
+        H_state = H @ QF.state
+        variance = abs(H_state.norm_squared() - energy * energy)
+
+        results.trajectory.append(E)
+        results.variances.append(variance)
+        log(f"step={step}, eigenvalue={E}, energy={energy}, variance={variance}")
+        if E < results.energy:
+            results.energy, results.state = E, QF.state.copy()
         if callback is not None:
-            callback(QF.state)
-        log(
-            f"step={step}, energy={newE}, change={oldE-newE}, {H.expectation(QF.state)}"
-        )
-        energies.append(newE)
-        if newE < best_energy:
-            best_energy, best_vector = newE, QF.state
-        if newE - oldE >= abs(tol) or newE - oldE >= -abs(
-            tol
-        ):  # This criteria makes it stop
-            msg = "Energy change below tolerance"
-            log(msg)
-            converged = True
+            callback(QF.state, results)
+
+        energy_change = E - last_E
+        if energy_change > abs(tol_up * E):
+            results.message = f"Energy fluctuation above tolerance {tol_up}"
+            results.converged = True
+            break
+        if -abs(tol * E) <= energy_change <= 0:
+            results.message = f"Energy decrease slower than tolerance {tol}"
+            results.converged = True
             break
         direction = -direction
-        oldE = newE
-    if not converged:
-        guess = CanonicalMPS(QF.state, center=0, normalize=True)
-        newE = H.expectation(guess).real
-        if callback is not None:
-            callback(QF.state)
-        energies.append(newE)
-        if newE < best_energy:
-            best_energy, best_vector = newE, QF.state
-        best_vector = CanonicalMPS(best_vector, center=0, normalize=True)
-    return OptimizeResults(
-        state=best_vector,
-        energy=best_energy,
-        converged=converged,
-        message=msg,
-        trajectory=energies,
-    )
+        last_E = E
+    return results
