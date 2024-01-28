5457b204eefe28cc73ee27cb200b2205c03ce354
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2024-01-28 19:20:18 +0100

    Fixed imports in optimization

--- src/seemps/optimization/__init__.py
+++ src/seemps/optimization/__init__.py
@@ -1,2 +1,4 @@
-from .descent import gradient_descent
-from .dmrg import dmrg, OptimizeResults
+from .descent import gradient_descent, OptimizeResults
+from .dmrg import dmrg
+from .arnoldi import arnoldi_eigh
+from .power import power_method
