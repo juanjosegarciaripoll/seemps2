5a36bd251b80de8471c5438030e119bbc0d17a32
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2024-02-21 11:20:18 +0100

    Added check rules to make.cmd

--- scripts/make.cmd
+++ scripts/make.cmd
@@ -12,3 +12,8 @@ goto :eof
 
 :install
 pip install --upgrade .
+
+:check
+mypy src/seemps
+flake8 src/seemps --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 src/seemps --count --exit-zero --max-complexity=10 --max-line-length=150 --ignore W503,E741,E203,C901 --statistics
