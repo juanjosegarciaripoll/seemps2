9364032a0268980ea7cfabbcd7854205e7b39552
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2024-03-24 17:10:40 +0100

    Implemented higher order finite difference formulas

--- src/seemps/analysis/finite_differences.py
+++ src/seemps/analysis/finite_differences.py
@@ -25,4 +25,110 @@ def mpo_combined(n, a, b, c, closed=True, **kwdargs):
 
 
 def finite_differences_mpo(n, Δx, closed=True, **kwdargs):
+    if n == 1:
+        raise Exception("finite_differences_mpo() does not work with length 1")
     return (1 / Δx**2) * mpo_combined(n, -2, 1, 1, closed=closed, **kwdargs)
+
+
+from ..register.transforms import mpo_weighted_shifts
+
+_filtered_differences = {
+    # First order derivatives
+    (1, 3): ([-1 / 2, 1 / 2], [1, -1]),
+    (1, 5): ([-1 / 8, -2 / 8, 2 / 8, 1 / 8], [2, 1, -1, -2]),
+    (1, 7): (
+        [-1 / 32, -4 / 32, -5 / 32, 5 / 32, 4 / 32, 1 / 32],
+        [3, 2, 1, -1, -2, -3],
+    ),
+    (1, 9): (
+        [
+            -1 / 128,
+            -6 / 128,
+            -14 / 128,
+            -14 / 128,
+            14 / 128,
+            14 / 128,
+            6 / 128,
+            1 / 128,
+        ],
+        [4, 3, 2, 1, -1, -2, -3, -4],
+    ),
+    (1, 11): (
+        [
+            -8 / 512,
+            -27 / 512,
+            -48 / 512,
+            -42 / 512,
+            42 / 512,
+            48 / 512,
+            27 / 512,
+            8 / 512,
+        ],
+        [4, 3, 2, 1, -1, -2, -3, -4],
+    ),
+    # Second order derivatives
+    (2, 3): ([1, -2, 1], [-1, 0, 1]),
+    (2, 5): ([1 / 4, -2 / 4, 1 / 4], [-2, 0, 2]),
+    (2, 7): (
+        [1 / 16, 2 / 16, -1 / 16, -4 / 16, -1 / 16, 2 / 16, 1 / 16],
+        [-3, -2, -1, 0, 1, 2, 3],
+    ),
+    (2, 9): (
+        [1 / 64, 4 / 64, 4 / 64, -4 / 64, -10 / 64, -4 / 64, 4 / 64, 4 / 64, 1 / 64],
+        [-4, -3, -2, -1, 0, 1, 2, 3, 4],
+    ),
+}
+
+
+def smooth_finite_differences_mpo(
+    L: int,
+    order: int,
+    filter: int = 3,
+    dx: float = 1.0,
+    periodic: bool = False,
+    base: int = 2,
+) -> MPO:
+    """Finite differences operator with noise resilience.
+    Create the operator that implements a finite-difference approximation to
+    the derivative of given `order` for a function encoded in `L`
+    units of dimension `base` (which defaults to 2 for qubits). It assumes
+    a uniformly spaced grid with separation `dx`.
+
+    Parameters
+    ----------
+    L : int
+        Number of elements in the quantum register
+    order : int
+        Order of the derivative (currently 1 or 2)
+    filter : int, default = 3
+        Size of the finite-difference formula with implicit filtering
+    dx : float, default = 1.0
+        Spacing of the grid
+    periodic : bool, default = False
+        Whether the grid assumes periodic boundary conditions
+    base : int, default = 2
+        Quantization of the tensor train (i.e. dimension of the register units)
+
+    Returns
+    -------
+    operator : MPO
+        Matrix product operator encoding the finite difference formula
+
+    Notes
+    -----
+    See http://www.holoborodko.com/pavel/numerical-methods/numerical-derivative/smooth-low-noise-differentiators
+    """
+    key = (order, filter)
+    weights, shifts = _filtered_differences.get(key, (None, None))
+    if weights is None:
+        raise ValueError(
+            "Unknown finite difference derivative of order {order} with noise filter of size {filter}"
+        )
+    else:
+        return mpo_weighted_shifts(
+            L,
+            np.asarray(weights) / (dx**order),
+            shifts,
+            periodic=periodic,
+            base=base,
+        )
--- tests/test_analysis/test_finite_differences.py
+++ tests/test_analysis/test_finite_differences.py
@@ -1,9 +1,13 @@
 import numpy as np
-from seemps.analysis.finite_differences import *
+from scipy.sparse import spdiags, csr_matrix, eye
+from seemps.analysis.finite_differences import (
+    finite_differences_mpo,
+    smooth_finite_differences_mpo,
+)
 from seemps.analysis.space import Space
 from seemps.state import MPS, DEFAULT_STRATEGY, NO_TRUNCATION
 
-from ..tools import *
+from .. import tools
 
 
 def gaussian(x):
@@ -31,7 +35,7 @@ def finite_differences_v(n, Δx, closed=True):
     )
 
 
-class TestFiniteDifferences(TestCase):
+class TestFiniteDifferences(tools.TestCase):
     def test_finite_differences(self):
         for n in range(2, 10):
             qubits_per_dimension = [n]
@@ -47,3 +51,119 @@ class TestFiniteDifferences(TestCase):
                 finite_differences_mpo(n, Δx, closed=True, strategy=NO_TRUNCATION) @ ψ
             )
             self.assertSimilar(fd_sol, fd_mps_sol)
+
+
+class TestFiniteDifferences(tools.TestCase):
+    def Down(self, nqubits: int, periodic: bool = False) -> csr_matrix:
+        """Moves f[i] to f[i-1]"""
+        L = 2**nqubits
+        if periodic:
+            M = spdiags([np.ones(L), np.ones(L)], [1, -L + 1], format="csr")
+        else:
+            M = spdiags([np.ones(L)], [1], format="csr")
+        return M
+
+    def Up(self, nqubits: int, periodic: bool = False) -> csr_matrix:
+        """Moves f[i] to f[i+1]"""
+        return self.Down(nqubits, periodic).T
+
+    def test_first_derivative_two_qubits_perodic(self):
+        dx = 0.1
+        D2 = smooth_finite_differences_mpo(
+            2, order=1, filter=3, periodic=True, dx=dx
+        ).to_matrix()
+        self.assertSimilar(
+            D2,
+            [
+                [0, 0.5 / dx, 0, -0.5 / dx],
+                [-0.5 / dx, 0, 0.5 / dx, 0],
+                [0, -0.5 / dx, 0, 0.5 / dx],
+                [0.5 / dx, 0, -0.5 / dx, 0],
+            ],
+        )
+        D = self.Down(2, periodic=True)
+        self.assertSimilar(D2, (D - D.T) / (2.0 * dx))
+
+    def test_second_derivative_two_qubits_perodic(self):
+        dx = 0.1
+        D2 = smooth_finite_differences_mpo(
+            2, order=2, filter=3, periodic=True, dx=dx
+        ).to_matrix()
+        dx2 = dx * dx
+        self.assertSimilar(
+            D2,
+            [
+                [-2 / dx2, 1 / dx2, 0, 1 / dx2],
+                [1 / dx2, -2 / dx2, 1 / dx2, 0],
+                [0, 1 / dx2, -2 / dx2, 1 / dx2],
+                [1 / dx2, 0, 1 / dx2, -2 / dx2],
+            ],
+        )
+        D = self.Down(2, periodic=True)
+        I = eye(4)
+        self.assertSimilar(D2, (D - 2 * I + D.T) / dx2)
+
+    def test_first_derivative_two_qubits_non_perodic(self):
+        dx = 0.1
+        D2 = smooth_finite_differences_mpo(
+            2, order=1, filter=3, periodic=False, dx=dx
+        ).to_matrix()
+        self.assertSimilar(
+            D2,
+            [
+                [0, 0.5 / dx, 0, 0],
+                [-0.5 / dx, 0, 0.5 / dx, 0],
+                [0, -0.5 / dx, 0, 0.5 / dx],
+                [0, 0, -0.5 / dx, 0],
+            ],
+        )
+        D = self.Down(2, periodic=False)
+        self.assertSimilar(D2, (D - D.T) / (2.0 * dx))
+
+    def test_second_derivative_two_qubits_perodic(self):
+        dx = 0.1
+        D2 = smooth_finite_differences_mpo(
+            2, order=2, filter=3, periodic=False, dx=dx
+        ).to_matrix()
+        dx2 = dx * dx
+        self.assertSimilar(
+            D2,
+            [
+                [-2 / dx2, 1 / dx2, 0, 0],
+                [1 / dx2, -2 / dx2, 1 / dx2, 0],
+                [0, 1 / dx2, -2 / dx2, 1 / dx2],
+                [0, 0, 1 / dx2, -2 / dx2],
+            ],
+        )
+        D = self.Down(2, periodic=False)
+        I = eye(4)
+        self.assertSimilar(D2, (D - 2 * I + D.T) / dx2)
+
+    def test_second_derivative_two_qubits_perodic(self):
+        dx = 0.1
+        D2 = smooth_finite_differences_mpo(
+            2, order=2, filter=3, periodic=False, dx=dx
+        ).to_matrix()
+        dx2 = dx * dx
+        self.assertSimilar(
+            D2,
+            [
+                [-2 / dx2, 1 / dx2, 0, 0],
+                [1 / dx2, -2 / dx2, 1 / dx2, 0],
+                [0, 1 / dx2, -2 / dx2, 1 / dx2],
+                [0, 0, 1 / dx2, -2 / dx2],
+            ],
+        )
+        D = self.Down(2, periodic=False)
+        I = eye(4)
+        self.assertSimilar(D2, (D - 2 * I + D.T) / dx2)
+
+    def test_second_derivative_two_qubits_smooth_and_ordinary(self):
+        dx = 1 / 4.0
+        for nqubits in range(2, 10):
+            for periodic in [False, True]:
+                D2a = smooth_finite_differences_mpo(
+                    nqubits, order=2, filter=3, periodic=periodic, dx=dx
+                )
+                D2b = finite_differences_mpo(nqubits, dx, closed=periodic)
+                self.assertSimilar(D2a.to_matrix(), D2b.to_matrix())
