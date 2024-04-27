daf5475cefee8d0fed117b1b43a9d373f3c8595c
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2024-04-27 11:51:35 +0200

    Make printing uniform with Python version

--- src/seemps/state/strategy.cc
+++ src/seemps/state/strategy.cc
@@ -5,6 +5,156 @@
 
 namespace seemps {
 
+Strategy::Strategy(int a_method, double a_tolerance,
+                   int a_simplification_method,
+                   double a_simplification_tolerance, size_t a_bond_dimension,
+                   int a_num_sweeps, bool a_normalize_flag)
+    : method{truncation_from_int(a_method)}, tolerance{a_tolerance},
+      simplification_method{simplification_from_int(a_simplification_method)},
+      simplification_tolerance{a_simplification_tolerance},
+      max_bond_dimension{a_bond_dimension}, max_sweeps{a_num_sweeps},
+      normalize{a_normalize_flag} {
+  if (tolerance < 0 || tolerance > 1.0) {
+    throw std::invalid_argument("Invalid Strategy tolerance");
+  }
+  if (tolerance == 0 && method != Truncation::DO_NOT_TRUNCATE) {
+    method = Truncation::ABSOLUTE_SINGULAR_VALUE;
+  }
+  if (simplification_tolerance < 0 || simplification_tolerance > 1.0) {
+    throw std::invalid_argument("Invalid Strategy tolerance");
+  }
+  if (max_sweeps <= 0) {
+    throw std::invalid_argument("Invalid Strategy maximum number of sweeps");
+  }
+}
+
+Strategy Strategy::replace(py::object a_method, py::object a_tolerance,
+                           py::object a_simplification_method,
+                           py::object a_simplification_tolerance,
+                           py::object a_bond_dimension, py::object a_num_sweeps,
+                           py::object a_normalize_flag) const {
+  Strategy output = *this;
+  if (!a_method.is_none()) {
+    output.method = truncation_from_int(py::int_(a_method));
+  }
+  if (!a_tolerance.is_none()) {
+    output.set_tolerance(py::cast<double>(a_tolerance));
+  }
+  if (!a_simplification_method.is_none()) {
+    output.set_simplification_method(
+        simplification_from_int(py::int_(a_simplification_method)));
+  }
+  if (!a_simplification_tolerance.is_none()) {
+    output.set_simplification_tolerance(py::float_(a_simplification_tolerance));
+  }
+  if (!a_bond_dimension.is_none()) {
+    output.set_max_bond_dimension(py::int_(a_bond_dimension));
+  }
+  if (!a_num_sweeps.is_none()) {
+    output.set_max_sweeps(py::int_(a_num_sweeps));
+  }
+  if (!a_normalize_flag.is_none()) {
+    output.normalize = py::cast<bool>(a_normalize_flag);
+  }
+  return output;
+}
+
+Truncation Strategy::truncation_from_int(int value) {
+  if (value < 0 || value > 3) {
+    throw std::invalid_argument("Invalid Strategy Truncation");
+  }
+  return static_cast<Truncation>(value);
+}
+
+Simplification Strategy::simplification_from_int(int value) {
+  if (value < 0 || value > 2) {
+    throw std::invalid_argument("Invalid Strategy Simplification");
+  }
+  return static_cast<Simplification>(value);
+}
+
+Strategy &Strategy::set_method(int value) {
+  method = truncation_from_int(value);
+  return *this;
+}
+
+Strategy &Strategy::set_simplification_method(int value) {
+  simplification_method = simplification_from_int(value);
+  return *this;
+}
+
+Strategy &Strategy::set_tolerance(double value) {
+  if (value >= 0 && value <= 1.0) {
+    if (value == 0 && method != Truncation::DO_NOT_TRUNCATE) {
+      method = Truncation::ABSOLUTE_SINGULAR_VALUE;
+    }
+    tolerance = value;
+    return *this;
+  }
+  throw std::invalid_argument("Invalid Strategy tolerance");
+}
+
+Strategy &Strategy::set_simplification_tolerance(double value) {
+  if (value >= 0 && value <= 1.0) {
+    simplification_tolerance = value;
+    return *this;
+  }
+  throw std::invalid_argument("Invalid Strategy simplification tolerance");
+}
+
+Strategy &Strategy::set_max_bond_dimension(size_t value) {
+  max_bond_dimension = value;
+  return *this;
+}
+
+Strategy &Strategy::set_max_sweeps(int value) {
+  if (value > 0) {
+    max_sweeps = value;
+    return *this;
+  }
+  throw std::invalid_argument("Invalid Strategy maximum number of sweeps");
+}
+
+std::string Strategy::truncation_name() const {
+  switch (method) {
+  case Truncation::DO_NOT_TRUNCATE:
+    return "None";
+  case Truncation::RELATIVE_SINGULAR_VALUE:
+    return "RelativeSVD";
+  case Truncation::RELATIVE_NORM_SQUARED_ERROR:
+    return "RelativeNorm";
+  case Truncation::ABSOLUTE_SINGULAR_VALUE:
+    return "AbsoluteSVD";
+  default:
+    throw std::runtime_error("Invalid truncation method found in Strategy");
+  }
+}
+
+std::string Strategy::simplification_name() const {
+  switch (simplification_method) {
+  case Simplification::DO_NOT_SIMPLIFY:
+    return "None";
+  case Simplification::CANONICAL_FORM:
+    return "CanonicalForm";
+  case Simplification::VARIATIONAL:
+    return "Variational";
+  default:
+    throw std::runtime_error("Invalid simplification method found in Strategy");
+  }
+}
+
+std::string Strategy::str() const {
+  std::ostringstream buffer;
+  buffer << "Strategy(method=" << truncation_name()
+         << ", tolerance=" << tolerance
+         << ", max_bond_dimension=" << max_bond_dimension
+         << ", normalize=" << (normalize ? "True" : "False")
+         << ", simplify=" << simplification_name()
+         << ", simplification_tolerance=" << simplification_tolerance
+         << ", max_sweeps=" << max_sweeps << ")";
+  return buffer.str();
+}
+
 static double _truncate_relative_norm_squared(const py::object &a,
                                               const Strategy &s) {
   static std::vector<double> buffer(1024, 0.0);
