6770298fcda637d7c86dff5ac72eae91db981c2f
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2024-04-30 15:41:42 +0200

    Simplify build file

--- setup.py
+++ setup.py
@@ -1,45 +1,8 @@
 from setuptools import find_packages, setup, Extension
+from Cython.Build import cythonize
 import glob
 import numpy as np
 import sys
-
-# This flag controls whether we build the library with bounds checks and
-# other safety measures.Useful when testing where a code breaks down;
-# but bad for production performance
-debug_library = False
-extra_compile_args = []
-from Cython.Build import cythonize
-import Cython.Compiler.Options
-
-# See https: // cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html
-# for a deeper explanation of the choices here
-# Cython.Compiler.Options.docstring = False
-Cython.Compiler.Options.error_on_uninitialized = True
-if not debug_library:
-    directives = {
-        "language_level": "3",  # We assume Python 3 code
-        "boundscheck": False,  # Do not check array access
-        "wraparound": False,  # a[-1] does not work
-        "always_allow_keywords": False,  # Faster calling conventions
-        "cdivision": True,  # No exception on zero denominator
-        "initializedcheck": False,  # We take care of initializing cdef classes and memory views
-        "overflowcheck": False,
-        "binding": False,
-    }
-    if sys.platform == "linux":
-        # We assume GCC or other compilers with compatible command line
-        extra_compile_args = ["-O3", "-ffast-math"]
-    else:
-        # We assume Microsoft Visual C / C++ compiler
-        extra_compile_args = ["/Ox", "/fp:fast"]
-else:
-    directives = {
-        "language_level": "3",  # We assume Python 3 code
-        "always_allow_keywords": False,  # Faster calling conventions
-        "boundscheck": True,
-        "initializedcheck": True,
-        "wraparound": False,
-    }
 
 # The main interface is through Pybind11Extension.
 # * You can add cxx_std = 11 / 14 / 17, and then build_ext can be removed.
@@ -51,11 +14,15 @@ else:
 # reproducible builds(https: // github.com/pybind/python_example/pull/53)
 from pybind11.setup_helpers import Pybind11Extension
 
-extra_compile_args = []
+if sys.platform == "linux":
+    # We assume GCC or other compilers with compatible command line
+    extra_compile_args = ["-O3", "-ffast-math", "-Wno-sign-compare"]
+else:
+    # We assume Microsoft Visual C / C++ compiler
+    extra_compile_args = ["/Ox", "/fp:fast"]
 extra_dependencies = [
     s.replace("\\", "/") for s in glob.glob("src/**/*.h", recursive=True)
 ] + [s.replace("\\", "/") for s in glob.glob("src/**/*.cc", recursive=True)]
-print(extra_dependencies)
 pybind11_modules = [
     Pybind11Extension(
         "seemps.state.core",
