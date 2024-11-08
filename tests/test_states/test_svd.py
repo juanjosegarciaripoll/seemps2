e5264678bb1cb893b35e16736cff8930211dca4e
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2024-11-07 16:28:36 -0800

    Avoid using _svd() directly

--- tests/test_states/test_svd.py
+++ tests/test_states/test_svd.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy.linalg
-from seemps.state.core import _svd
+from seemps.state.schmidt import _destructive_svd
 from .. import tools
 
 
@@ -10,18 +10,18 @@ class TestSVD(tools.TestCase):
             for n in range(1, max_size):
                 yield (m, n)
 
-    def test_real_svd(self):
+    def test_real_destructive_svd(self):
         for s in self.size_iterator():
             A = self.rng.normal(size=s)
             U, s, VT = scipy.linalg.svd(A, full_matrices=False, compute_uv=True)
             self.assertSimilar(A, (U * s) @ VT, rtol=1e-10, atol=1e-15)
-            U, s, VT = _svd(A.copy())
+            U, s, VT = _destructive_svd(A.copy())
             self.assertSimilar(A, (U * s) @ VT, rtol=1e-10, atol=1e-15)
 
-    def test_complex_svd(self):
+    def test_complex_destructive_svd(self):
         for s in self.size_iterator():
             A = self.rng.normal(size=s) + 1j * self.rng.normal(size=s)
             U, s, VT = scipy.linalg.svd(A, full_matrices=False, compute_uv=True)
             self.assertSimilar(A, (U * s) @ VT, rtol=1e-10, atol=1e-15)
-            U, s, VT = _svd(A.copy())
+            U, s, VT = _destructive_svd(A.copy())
             self.assertSimilar(A, (U * s) @ VT, rtol=1e-10, atol=1e-15)
