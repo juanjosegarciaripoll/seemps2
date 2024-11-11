7707078da0f73a1653d09372eecbd1867c51c79c
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2024-11-10 20:02:29 -0800

    Restore automatic conversion of tuples

--- src/seemps/state/core.cc
+++ src/seemps/state/core.cc
@@ -1,7 +1,9 @@
 // Here we initialize the MY_PyArray_API
 #define INIT_NUMPY_ARRAY_CPP
-#include <nanobind/operators.h>
 #include "core.h"
+#include <nanobind/operators.h>
+#include <nanobind/stl/string.h>
+#include <nanobind/stl/tuple.h>
 #include "tensors.h"
 #include "strategy.h"
 #include "mps.h"
@@ -35,10 +37,7 @@ NB_MODULE(core, m) {
 
   m.def("_contract_nrjl_ijk_klm", &contract_nrjl_ijk_klm);
 
-  m.def("_destructive_svd", [](py::object A) {
-    auto t = destructive_svd(A);
-    return py::make_tuple(std::get<0>(t), std::get<1>(t), std::get<2>(t));
-  });
+  m.def("_destructive_svd", destructive_svd);
 
   m.def("_select_svd_driver", &select_svd_driver);
 
