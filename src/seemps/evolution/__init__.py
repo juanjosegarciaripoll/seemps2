521455d2f4b5b8831fc4300d7b8f916b3fcae6ab
Jorge Gidi <jorgegidi@gmail.com>
2025-07-23 15:06:40 -0400

    add Radau IIA integrator of orders 3 and 5

--- src/seemps/evolution/__init__.py
+++ src/seemps/evolution/__init__.py
@@ -1,6 +1,7 @@
 from .arnoldi import arnoldi
 from .euler import euler
+from .radau import radau
 from .runge_kutta import runge_kutta, runge_kutta_fehlberg
 from . import trotter
 
-__all__ = ["arnoldi", "euler", "runge_kutta", "runge_kutta_fehlberg", "trotter"]
+__all__ = ["arnoldi", "euler", "runge_kutta", "runge_kutta_fehlberg", "trotter", 'radau']
