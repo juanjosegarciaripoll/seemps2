2c552c56677ebdb6e92e9ef355c0dcf6f9093063
Jorge Gidi <jorgegidi@gmail.com>
2025-12-01 11:54:11 -0300

    add tests for TDVP

---
+++ tests/test_evolution/test_tdvp.py
@@ -0,0 +1,40 @@
+import numpy as np
+from math import sqrt
+from seemps.state import product_state
+from seemps.evolution import tdvp
+from seemps.hamiltonians import HeisenbergHamiltonian
+from .problem import EvolutionTestCase
+
+class TestTDVP(EvolutionTestCase):
+    def test_tdvp_time_steps_and_callback(self):
+        """Check the integration times used by the algorithm"""
+        nqubits = 4
+        mps = product_state([np.ones(2) / sqrt(2)] * nqubits)
+        H = HeisenbergHamiltonian(nqubits).to_mpo()
+
+        final = tdvp(H, 1.0, mps, steps=10, callback=lambda t, state: t)
+        self.assertSimilar(final, np.linspace(0, 1.0, 11))
+
+        final = tdvp(H, (1.0, 2.0), mps, steps=10, callback=lambda t, state: t)
+        self.assertSimilar(final, np.linspace(1.0, 2.0, 11))
+
+        t_span = np.linspace(1.0, 3.0, 13)
+        final = tdvp(H, t_span, mps, steps=10, callback=lambda t, state: t)
+        self.assertSimilar(final, t_span)
+
+    def test_tdvp_accumulated_phase(self):
+        """Evolve with a state that is invariant under the Hamiltonian
+        and check the accumulated phase."""
+        T = 0.01
+        steps = 1
+        dt = T / steps
+        nqubits = 4
+        mps = product_state([np.ones(2) / sqrt(2)] * nqubits)
+
+        H = HeisenbergHamiltonian(nqubits).to_mpo()
+        final = tdvp(H, T, mps, steps=steps)
+        self.assertSimilarStates(final, mps)
+
+        E = H.expectation(mps)
+        phase = np.exp(-1j * dt * E * steps)
+        self.assertSimilar(final, phase * mps)
