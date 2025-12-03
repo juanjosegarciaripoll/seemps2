a4b87751787df4dbecc19051828c017e2a4e810b
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2025-12-03 10:42:34 +0100

    Remove unused classes and functions in tests

--- tests/test_analysis/test_finite_differences.py
+++ tests/test_analysis/test_finite_differences.py
@@ -5,33 +5,7 @@ from seemps.analysis.finite_differences import (
     finite_differences_mpo,
     smooth_finite_differences_mpo,
 )
-
 from .. import tools
-
-
-def gaussian(x):
-    f = np.exp(-(x**2))
-    return f / np.linalg.norm(f)
-
-
-def S_plus_v(n, closed=True):
-    S = np.diag(np.ones(2**n - 1), +1)
-    if closed:
-        S[-1, 0] = 1
-    return S
-
-
-def S_minus_v(n, closed=True):
-    S = np.diag(np.ones(2**n - 1), -1)
-    if closed:
-        S[0, -1] = 1
-    return S
-
-
-def finite_differences_v(n, Δx, closed=True):
-    return (1 / Δx**2) * (
-        S_plus_v(n, closed=closed) + S_minus_v(n, closed=closed) - 2 * np.eye(2**n)
-    )
 
 
 class TestFiniteDifferences(tools.TestCase):
@@ -39,14 +13,10 @@ class TestFiniteDifferences(tools.TestCase):
         """Moves f[i] to f[i-1]"""
         L = 2**nqubits
         if periodic:
-            M = spdiags([np.ones(L), np.ones(L)], [1, -L + 1], format="csr") # type: ignore # scipy-stubs broken
+            M = spdiags([np.ones(L), np.ones(L)], [1, -L + 1], format="csr")  # type: ignore # scipy-stubs broken
         else:
-            M = spdiags([np.ones(L)], [1], format="csr") # type: ignore # scipy-stubs broken
+            M = spdiags([np.ones(L)], [1], format="csr")  # type: ignore # scipy-stubs broken
         return cast(csr_matrix, M)
-
-    def Up(self, nqubits: int, periodic: bool = False) -> csr_matrix:
-        """Moves f[i] to f[i+1]"""
-        return self.Down(nqubits, periodic).T.tocsr()
 
     def test_first_derivative_two_qubits_perodic(self):
         dx = 0.1
