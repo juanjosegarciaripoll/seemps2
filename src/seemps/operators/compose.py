824fd4678f03691d16491a431966d18583185947
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2025-12-26 15:52:30 +0100

    Added mpo_tensor_product

---
+++ src/seemps/operators/compose.py
@@ -0,0 +1,17 @@
+from ..state import mps_tensor_product, Strategy
+from ..typing import MPSOrder
+from .mpo import MPO
+from .simplify_mpo import mps_as_mpo, mpo_as_mps
+
+
+def mpo_tensor_product(
+    mpo_list: list[MPO],
+    mpo_order: MPSOrder = "A",
+    strategy: Strategy | None = None,
+    simplify_steps: bool = False,
+) -> MPO:
+    return mps_as_mpo(
+        mps_tensor_product(
+            [mpo_as_mps(O) for O in mpo_list], mpo_order, strategy, simplify_steps
+        )
+    )
