d3b10be7779550a0a555d9e239bb0dcc863307c1
Jorge Gidi <jorgegidi@gmail.com>
2025-12-29 20:22:04 -0300

    reuse dmrg tools from tdvp

--- src/seemps/evolution/tdvp.py
+++ src/seemps/evolution/tdvp.py
@@ -1,107 +1,37 @@
 from __future__ import annotations
 
 import numpy as np
-from typing import Any
-import scipy.sparse.linalg
 from scipy.sparse.linalg import expm_multiply
 from seemps.state import MPS, CanonicalMPS, Strategy, DEFAULT_STRATEGY
 from seemps.cython import _contract_last_and_first
-from seemps.optimization.dmrg import QuadraticForm, DMRGMatrixOperator
+from seemps.optimization.dmrg import (
+    QuadraticForm,
+    DMRGMatrixOperator,
+    OneSiteDMRGOperator,
+)
 from seemps.operators import MPO
 from seemps.evolution.common import ode_solver, ODECallback, TimeSpan
 
 
-class OneSiteTDVPOperator(scipy.sparse.linalg.LinearOperator):
-    L: np.ndarray
-    H_mpo: np.ndarray
-    R: np.ndarray
-    v_shape: tuple[int, int, int]
-
-    def __new__(cls, *args: Any, **kwargs: Any):
-        return object.__new__(cls)
-
-    def __init__(self, L: np.ndarray, H: np.ndarray, R: np.ndarray):
-        self.L = L
-        self.H_mpo = H
-        self.R = R
-        _, _, b = L.shape
-        _, _, f = R.shape
-        _, _, k, _ = H.shape
-        self.v_shape = (b, k, f)
-
-        super().__init__(dtype=L.dtype, shape=(b * k * f, b * k * f))  # type: ignore[call-arg] # pyright: ignore[reportCallIssue]
-
-    def _matvec(self, v: np.ndarray) -> np.ndarray:
-        v = v.reshape(self.v_shape)
-
-        aux = np.tensordot(v, self.L, axes=(0, 2))
-        aux = np.tensordot(aux, self.H_mpo, axes=([0, 3], [2, 0]))
-        aux = np.tensordot(aux, self.R, axes=([0, 3], [2, 1]))
-
-        return aux.reshape(-1)
-
-    def _rmatvec(self, v: np.ndarray) -> np.ndarray:
-        a = self.L.shape[0]
-        g = self.H_mpo.shape[1]
-        d = self.R.shape[0]
-        v = v.reshape(a, g, d)
-
-        aux = np.tensordot(v, self.L.conj(), axes=(0, 0))
-        aux = np.tensordot(aux, self.H_mpo.conj(), axes=([0, 2], [1, 0]))
-        aux = np.tensordot(aux, self.R.conj(), axes=([0, 3], [0, 1]))
-
-        return aux.reshape(-1)
-
-    def trace(self) -> complex:
-        l_c = np.trace(self.L, axis1=0, axis2=2)
-        w_ce = np.trace(self.H_mpo, axis1=1, axis2=2)
-        r_e = np.trace(self.R, axis1=0, axis2=2)
-        return np.dot(l_c, np.dot(w_ce, r_e))
-
-
-class TwoSiteTDVPOperator(DMRGMatrixOperator):
-    def __new__(cls, *args: Any, **kwargs: Any):
-        return object.__new__(cls)
-
-    def __init__(self, L: np.ndarray, H12: np.ndarray, R: np.ndarray):
-        super().__init__(L, H12, R)
-
-    def _rmatvec(self, v: np.ndarray) -> np.ndarray:
-        a = self.L.shape[0]
-        i = self.H12.shape[1]
-        j = self.H12.shape[3]
-        e = self.R.shape[0]
-
-        v = v.reshape(a, i, j, e)
-        aux = np.tensordot(v, self.L.conj(), axes=(0, 0))
-        aux = np.tensordot(aux, self.H12.conj(), axes=([3, 0, 1], [0, 1, 3]))
-        aux = np.tensordot(aux, self.R.conj(), axes=([0, 4], [0, 1]))
-
-        return aux.reshape(-1)
-
-    def trace(self) -> complex:
-        l_c = np.trace(self.L, axis1=0, axis2=2)
-        tmp = np.trace(self.H12, axis1=1, axis2=2)
-        w_ce = np.trace(tmp, axis1=1, axis2=2)
-        r_e = np.trace(self.R, axis1=0, axis2=2)
-        return np.dot(l_c, np.dot(w_ce, r_e))
-
-
 class TDVPForm(QuadraticForm):
-    def one_site_Hamiltonian(self, i: int) -> OneSiteTDVPOperator:
-        return OneSiteTDVPOperator(self.left_env[i], self.H[i], self.right_env[i])
+    def one_site_Hamiltonian(self, i: int) -> OneSiteDMRGOperator:
+        return OneSiteDMRGOperator(  # pyright: ignore[reportCallIssue]
+            self.left_env[i],  # pyright: ignore[reportArgumentType]
+            self.H[i],  # pyright: ignore[reportArgumentType]
+            self.right_env[i],  # pyright: ignore[reportArgumentType]
+        )
 
-    def two_site_Hamiltonian(self, i: int) -> TwoSiteTDVPOperator:
+    def two_site_Hamiltonian(self, i: int) -> DMRGMatrixOperator:
         assert i == self.site
-        return TwoSiteTDVPOperator(
-            self.left_env[i],
-            _contract_last_and_first(self.H[i], self.H[i + 1]),
-            self.right_env[i + 1],
+        return DMRGMatrixOperator(  # pyright: ignore[reportCallIssue]
+            self.left_env[i],  # pyright: ignore[reportArgumentType]
+            _contract_last_and_first(self.H[i], self.H[i + 1]),  # pyright: ignore[reportArgumentType]
+            self.right_env[i + 1],  # pyright: ignore[reportArgumentType]
         )
 
 
 def _evolve(
-    operator: OneSiteTDVPOperator | TwoSiteTDVPOperator,
+    operator: OneSiteDMRGOperator | DMRGMatrixOperator,
     tensor: np.ndarray,
     factor: float | complex,
     normalize: bool = False,
