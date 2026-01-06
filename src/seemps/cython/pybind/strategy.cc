c237c1288f38a7eeacbf4ab50c29217093ed4db3
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2026-01-06 21:34:47 +0100

    Remove redundant file. Do not normalize in truncations

--- src/seemps/cython/pybind/strategy.cc
+++ src/seemps/cython/pybind/strategy.cc
@@ -33,10 +33,11 @@ static double _truncate_relative_norm_squared(const py::object &a,
   }
   final_size = std::min(final_size, s.get_max_bond_dimension());
   max_error = errors[N - final_size];
-
+  /*
   if (s.get_normalize_flag()) {
     _normalize(data_start, final_size, std::sqrt(total - max_error));
   }
+  */
   if (final_size < N) {
     vector_resize_in_place(a, final_size);
   }
@@ -65,9 +66,11 @@ static double _truncate_absolute_singular_value(const py::object &a,
   for (size_t i = final_size; i < N; ++i) {
     max_error += data[i] * data[i];
   }
+  /*
   if (s.get_normalize_flag()) {
     _normalize(data, final_size);
   }
+  */
   if (final_size < N) {
     vector_resize_in_place(a, final_size);
   }
