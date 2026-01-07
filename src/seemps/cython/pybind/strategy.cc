e78cad89a28c87101207fe09391797de7af785e3
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2026-01-07 19:22:24 +0100

    Strategy class takes ownership of truncation methods

--- src/seemps/cython/pybind/strategy.cc
+++ src/seemps/cython/pybind/strategy.cc
@@ -6,8 +6,15 @@
 namespace seemps
 {
 
-static double
-_truncate_relative_norm_squared(const py::object& a, const Strategy& s)
+// Static member function implementations
+double
+Strategy::truncate_do_not_truncate(const py::object& a, const Strategy& s)
+{
+  return 0.0;
+}
+
+double
+Strategy::truncate_relative_norm_squared(const py::object& a, const Strategy& s)
 {
   static std::vector<double> buffer(1024, 0.0);
   size_t N = array_size(a);
@@ -43,12 +50,13 @@ _truncate_relative_norm_squared(const py::object& a, const Strategy& s)
   return max_error;
 }
 
-static double
-_truncate_absolute_singular_value(const py::object& a, const Strategy& s,
-                                  double max_error)
+double
+Strategy::truncate_absolute_singular_value(const py::object& a,
+                                           const Strategy& s)
 {
   double* data = array_data<double>(a);
   size_t N = seemps::array_size(a);
+  double max_error = s.get_tolerance();
 
   size_t final_size = N;
   for (size_t i = 0; i < N; ++i)
@@ -85,27 +93,75 @@ _truncate_absolute_singular_value(const py::object& a, const Strategy& s,
 }
 
 double
+Strategy::truncate_relative_singular_value(const py::object& a,
+                                           const Strategy& s)
+{
+  double* data = array_data<double>(a);
+  size_t N = seemps::array_size(a);
+  double max_error = s.get_tolerance() * data[0];
+
+  size_t final_size = N;
+  for (size_t i = 0; i < N; ++i)
+    {
+      if (data[i] <= max_error)
+        {
+          final_size = i;
+          break;
+        }
+    }
+  if (final_size == 0)
+    {
+      final_size = 1;
+    }
+  else
+    {
+      final_size = std::min(final_size, s.get_max_bond_dimension());
+    }
+  max_error = 0.0;
+  for (size_t i = final_size; i < N; ++i)
+    {
+      max_error += data[i] * data[i];
+    }
+  /*
+  if (s.get_normalize_flag()) {
+    _normalize(data, final_size);
+  }
+  */
+  if (final_size < N)
+    {
+      vector_resize_in_place(a, final_size);
+    }
+  return max_error;
+}
+
+// Helper function to select the appropriate truncation function
+Strategy::TruncationFunction
+Strategy::select_truncation_function() const
+{
+  switch (method)
+    {
+    case Truncation::DO_NOT_TRUNCATE:
+      return &Strategy::truncate_do_not_truncate;
+    case Truncation::RELATIVE_NORM_SQUARED_ERROR:
+      return &Strategy::truncate_relative_norm_squared;
+    case Truncation::RELATIVE_SINGULAR_VALUE:
+      return &Strategy::truncate_relative_singular_value;
+    case Truncation::ABSOLUTE_SINGULAR_VALUE:
+      return &Strategy::truncate_absolute_singular_value;
+    default:
+      return &Strategy::truncate_do_not_truncate;
+    }
+}
+
+double
 destructively_truncate_vector(const py::object a, const Strategy& s)
 {
   if (!PyArray_Check(a.ptr()))
     {
-      throw std::invalid_argument("truncate_vector expected an ndarray");
+      throw std::invalid_argument(
+          "destructively_truncate_vector expected an ndarray");
     }
-  switch (s.get_method())
-    {
-    case Truncation::RELATIVE_NORM_SQUARED_ERROR:
-      return _truncate_relative_norm_squared(a, s);
-    case Truncation::RELATIVE_SINGULAR_VALUE:
-      {
-        return _truncate_absolute_singular_value(
-            a, s, s.get_tolerance() * array_data<double>(a)[0]);
-      }
-    case Truncation::ABSOLUTE_SINGULAR_VALUE:
-      return _truncate_absolute_singular_value(a, s, s.get_tolerance());
-    case Truncation::DO_NOT_TRUNCATE:
-    default:
-      return 0.0;
-    }
+  return s.truncate(a);
 }
 
 } // namespace seemps
