77f0775bd57e8414c3e07365c26a56710fe74933
Jorge Gidi <jorgegidi@gmail.com>
2026-01-09 21:49:13 -0300

    fix deprecated MPO.dimensions() call

--- src/seemps/optimization/power.py
+++ src/seemps/optimization/power.py
@@ -67,7 +67,7 @@ def power_method(
         identity = MPO([np.eye(d).reshape(1, d, d, 1) for d in H.physical_dimensions()])
         H = (H + shift * identity).join()
     state = CanonicalMPS(
-        random_mps(H.dimensions(), D=2) if guess is None else guess,
+        random_mps(H.physical_dimensions(), D=2) if guess is None else guess,
         strategy=strategy,
     )
     results = PowerMethodOptimizeResults(
