e0bd8dd43905ecf8ae124eb6a26fa730057c9b2a
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2026-01-10 16:58:09 +0100

    Also export and document euler2

--- src/seemps/evolution/__init__.py
+++ src/seemps/evolution/__init__.py
@@ -1,6 +1,6 @@
 from .arnoldi import arnoldi
 from .crank_nicolson import crank_nicolson
-from .euler import euler
+from .euler import euler, euler2
 from .radau import radau
 from .runge_kutta import runge_kutta, runge_kutta_fehlberg
 from .tdvp import tdvp
@@ -12,6 +12,7 @@ __all__ = [
     "arnoldi",
     "crank_nicolson",
     "euler",
+    "euler2",
     "runge_kutta",
     "runge_kutta_fehlberg",
     "tdvp",
