ce4bce2361e219f5348db483f912049bf22ff326
Juan Jose Garcia-Ripoll <juanjose.garciaripoll@gmail.com>
2026-03-29 19:07:57 +0200

    Test tensor sum uncovered branches

--- tests/test_states/test_tensor_sum.py
+++ tests/test_states/test_tensor_sum.py
@@ -1,5 +1,5 @@
 import numpy as np
-from seemps.state import NO_TRUNCATION, mps_tensor_sum
+from seemps.state import MPS, NO_TRUNCATION, mps_tensor_sum
 from ..tools import SeeMPSTestCase
 
 
@@ -53,3 +53,30 @@ class TestMPSOperations(SeeMPSTestCase):
             np.kron(A.to_vector(), np.ones(B_v.shape))
             + np.kron(np.ones(A_v.shape), B.to_vector()),
         )
+
+    def test_tensor_sum_single_site_terms_A_order(self):
+        A = MPS.from_vector(np.array([1.0, 2.0j]), [2], normalize=False)
+        B = MPS.from_vector(np.array([-1.0, 0.5]), [2], normalize=False)
+        C = mps_tensor_sum([A, B], mps_order="A", strategy=NO_TRUNCATION)
+        expected = np.kron(A.to_vector(), np.ones(2)) + np.kron(np.ones(2), B.to_vector())
+        self.assertSimilar(C.to_vector(), expected)
+
+    def test_tensor_sum_three_states_B_order_with_step_simplification(self):
+        A = MPS.from_vector(np.array([1.0, 2.0j]), [2], normalize=False)
+        B = MPS.from_vector(np.array([1.0, -1.0j, 2.0]), [3], normalize=False)
+        C = MPS.from_vector(np.array([0.5, -2.0]), [2], normalize=False)
+        result = mps_tensor_sum(
+            [A, B, C], mps_order="B", strategy=NO_TRUNCATION, simplify_steps=True
+        )
+        expected = (
+            np.kron(np.kron(A.to_vector(), np.ones(3)), np.ones(2))
+            + np.kron(np.kron(np.ones(2), B.to_vector()), np.ones(2))
+            + np.kron(np.kron(np.ones(2), np.ones(3)), C.to_vector())
+        )
+        self.assertEqual(result.physical_dimensions(), [2, 3, 2])
+        self.assertSimilar(result.to_vector(), expected)
+
+    def test_tensor_sum_rejects_invalid_order(self):
+        A = self.random_mps([2, 3])
+        with self.assertRaises(ValueError):
+            mps_tensor_sum([A, A], mps_order="invalid", strategy=NO_TRUNCATION)
